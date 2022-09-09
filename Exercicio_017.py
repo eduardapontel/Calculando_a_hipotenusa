e786f08d8e8460966feee28a81d57abff3b1e24a
Eduarda Alves Pontel <109531518+eduardapontel@users.noreply.github.com>
2022-09-09 09:04:28 -0300

    Add files via upload

--- Exercicio_017.py
+++ Exercicio_017.py
@@ -6,7 +6,20 @@ print('{:^35}'.format('CALCULANDO A HIPOTENUSA'))
 print(bordas)
 print()
 
-co = float(input('Digite o comprimento do cateto oposto: '))
-ca = float(input('Digite o comprimento do cateto adjacente: '))
+co = ''
+ca = ''
+
+while type(ca) != float:
+    try:
+        ca = float(input('Digite o comprimento do cateto oposto: '))
+    except:
+        print('Por favor digite um comprimento válido!')
+
+while type(co) != float:
+    try:
+        co = float(input('Digite o comprimento do cateto adjacente: '))
+    except:
+        print('Por favor digite um comprimento válido: ')
+
 # print('A hipotenusa é igual a {:.2f}'.format((co**2+ca**2)**(1/2)))
-print('A hipotenusa é igual a {:.2f}'.format(hypot(co, ca)))
+print('A hipotenusa tem o comprimento igual a {:.2f}'.format(hypot(co, ca)))
